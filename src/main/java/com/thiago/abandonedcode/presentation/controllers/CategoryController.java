4e7d646427aa38c3fc2e93cfdfbe957485cd478d
Thiagooms <thiagopaulista50@gmail.com>
2025-12-20 23:22:54 -0300

    [feature]: Add controllers

---
+++ src/main/java/com/thiago/abandonedcode/presentation/controllers/CategoryController.java
@@ -0,0 +1,9 @@
+package com.thiago.abandonedcode.presentation.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+}
---
+++ src/main/java/com/thiago/abandonedcode/presentation/controllers/PostController.java
@@ -0,0 +1,9 @@
+package com.thiago.abandonedcode.presentation.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+}
