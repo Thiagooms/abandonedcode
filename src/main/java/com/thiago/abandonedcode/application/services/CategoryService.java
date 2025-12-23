d4208a668687238ae4aad260883ccd72685535b0
Thiagooms <thiagopaulista50@gmail.com>
2025-12-23 12:28:01 -0300

    [feature]: Implement CategoryService and PostService use cases

--- src/main/java/com/thiago/abandonedcode/application/services/CategoryService.java
+++ src/main/java/com/thiago/abandonedcode/application/services/CategoryService.java
@@ -1,3 +1,37 @@
 package com.thiago.abandonedcode.application.services;
 
-public class CategoryService {}
+import com.thiago.abandonedcode.domain.entities.Category;
+import com.thiago.abandonedcode.domain.exceptions.CategoryNotFoundException;
+import com.thiago.abandonedcode.domain.exceptions.DuplicateSlugException;
+import com.thiago.abandonedcode.domain.ports.input.CreateCategoryUseCase;
+import com.thiago.abandonedcode.domain.ports.output.CategoryRepository;
+
+public class CategoryService implements CreateCategoryUseCase {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Category execute(String name, Long parentId) {
+        if (parentId != null) {
+            categoryRepository.findById(parentId)
+                    .orElseThrow(() -> CategoryNotFoundException.withId(parentId));
+        }
+
+        Category category;
+        if (parentId != null) {
+            category = new Category(name, parentId);
+        } else {
+            category = new Category(name);
+        }
+
+        if (categoryRepository.existsBySlug(category.getSlug().value())) {
+            throw DuplicateSlugException.forSlug(category.getSlug().value());
+        }
+
+        return categoryRepository.save(category);
+    }
+}
--- src/main/java/com/thiago/abandonedcode/application/services/PostService.java
+++ src/main/java/com/thiago/abandonedcode/application/services/PostService.java
@@ -1,3 +1,48 @@
 package com.thiago.abandonedcode.application.services;
 
-public class PostService {}
+import com.thiago.abandonedcode.domain.entities.Category;
+import com.thiago.abandonedcode.domain.entities.Post;
+import com.thiago.abandonedcode.domain.exceptions.CategoryNotFoundException;
+import com.thiago.abandonedcode.domain.exceptions.DuplicateSlugException;
+import com.thiago.abandonedcode.domain.ports.input.CreatePostUseCase;
+import com.thiago.abandonedcode.domain.ports.output.CategoryRepository;
+import com.thiago.abandonedcode.domain.ports.output.PostRepository;
+import com.thiago.abandonedcode.domain.valueobjects.Content;
+import com.thiago.abandonedcode.domain.valueobjects.Excerpt;
+
+public class PostService implements CreatePostUseCase {
+
+    private final PostRepository postRepository;
+    private final CategoryRepository categoryRepository;
+
+    public PostService(PostRepository postRepository, CategoryRepository categoryRepository) {
+        this.postRepository = postRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public Post execute(String title, String content, String excerpt, Long categoryId) {
+        Category category = null;
+        if (categoryId != null) {
+            category = categoryRepository.findById(categoryId)
+                    .orElseThrow(() -> CategoryNotFoundException.withId(categoryId));
+        }
+
+        Content contentVo = new Content(content);
+
+        Excerpt excerptVo;
+        if (excerpt != null && !excerpt.isBlank()) {
+            excerptVo = new Excerpt(excerpt);
+        } else {
+            excerptVo = Excerpt.fromContent(contentVo);
+        }
+
+        Post post = new Post(title, contentVo, excerptVo, category);
+
+        if (postRepository.existsBySlug(post.getSlug())) {
+            throw DuplicateSlugException.forSlug(post.getSlug().value());
+        }
+
+        return postRepository.save(post);
+    }
+}
