b1c33c8c9ec07e86391ce7edc81d04a1c9c4d210
Thiagooms <thiagopaulista50@gmail.com>
2025-12-23 12:28:10 -0300

    [feature]: Implement REST endpoints for posts and categories

--- src/main/java/com/thiago/abandonedcode/presentation/controllers/CategoryController.java
+++ src/main/java/com/thiago/abandonedcode/presentation/controllers/CategoryController.java
@@ -1,9 +1,33 @@
 package com.thiago.abandonedcode.presentation.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.thiago.abandonedcode.application.mappers.CategoryDtoMapper;
+import com.thiago.abandonedcode.domain.entities.Category;
+import com.thiago.abandonedcode.domain.ports.input.CreateCategoryUseCase;
+import com.thiago.abandonedcode.presentation.dto.CategoryRequest;
+import com.thiago.abandonedcode.presentation.dto.CategoryResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
+
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = createCategoryUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CategoryRequest request) {
+        Category category = createCategoryUseCase.execute(
+                request.name(),
+                request.parentId()
+        );
+
+        CategoryResponse response = CategoryDtoMapper.toResponse(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
--- src/main/java/com/thiago/abandonedcode/presentation/controllers/PostController.java
+++ src/main/java/com/thiago/abandonedcode/presentation/controllers/PostController.java
@@ -1,9 +1,35 @@
 package com.thiago.abandonedcode.presentation.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.thiago.abandonedcode.application.mappers.PostDtoMapper;
+import com.thiago.abandonedcode.domain.entities.Post;
+import com.thiago.abandonedcode.domain.ports.input.CreatePostUseCase;
+import com.thiago.abandonedcode.presentation.dto.PostRequest;
+import com.thiago.abandonedcode.presentation.dto.PostResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+
+    private final CreatePostUseCase createPostUseCase;
+
+    public PostController(CreatePostUseCase createPostUseCase) {
+        this.createPostUseCase = createPostUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostResponse> create(@Valid @RequestBody PostRequest request) {
+        Post post = createPostUseCase.execute(
+                request.title(),
+                request.content(),
+                request.excerpt(),
+                request.categoryId()
+        );
+
+        PostResponse response = PostDtoMapper.toResponse(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
