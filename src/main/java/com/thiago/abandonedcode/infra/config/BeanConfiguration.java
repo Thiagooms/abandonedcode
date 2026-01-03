179ec97a62668cad7bcfb3459245a98838356e7f
Thiagooms <thiagopaulista50@gmail.com>
2026-01-02 21:02:23 -0300

    [feature]: Configuration CORS for consuming the API on the front end.

--- src/main/java/com/thiago/abandonedcode/infra/config/BeanConfiguration.java
+++ src/main/java/com/thiago/abandonedcode/infra/config/BeanConfiguration.java
@@ -4,6 +4,16 @@ import com.thiago.abandonedcode.application.services.CategoryService;
 import com.thiago.abandonedcode.application.services.PostService;
 import com.thiago.abandonedcode.domain.ports.input.CreateCategoryUseCase;
 import com.thiago.abandonedcode.domain.ports.input.CreatePostUseCase;
+import com.thiago.abandonedcode.domain.ports.input.DeleteCategoryUseCase;
+import com.thiago.abandonedcode.domain.ports.input.DeletePostUseCase;
+import com.thiago.abandonedcode.domain.ports.input.GetCategoryByPathUseCase;
+import com.thiago.abandonedcode.domain.ports.input.GetCategoryUseCase;
+import com.thiago.abandonedcode.domain.ports.input.GetPostUseCase;
+import com.thiago.abandonedcode.domain.ports.input.ListCategoriesUseCase;
+import com.thiago.abandonedcode.domain.ports.input.ListPostsUseCase;
+import com.thiago.abandonedcode.domain.ports.input.PublishPostUseCase;
+import com.thiago.abandonedcode.domain.ports.input.UpdateCategoryUseCase;
+import com.thiago.abandonedcode.domain.ports.input.UpdatePostUseCase;
 import com.thiago.abandonedcode.domain.ports.output.CategoryRepository;
 import com.thiago.abandonedcode.domain.ports.output.PostRepository;
 import org.springframework.context.annotation.Bean;
@@ -13,15 +23,75 @@ import org.springframework.context.annotation.Configuration;
 public class BeanConfiguration {
 
     @Bean
-    public CreateCategoryUseCase createCategoryUseCase(CategoryRepository categoryRepository) {
+    public CategoryService categoryService(CategoryRepository categoryRepository) {
         return new CategoryService(categoryRepository);
     }
 
     @Bean
-    public CreatePostUseCase createPostUseCase(
+    public CreateCategoryUseCase createCategoryUseCase(CategoryService categoryService) {
+        return categoryService;
+    }
+
+    @Bean
+    public PostService postService(
             PostRepository postRepository,
             CategoryRepository categoryRepository
     ) {
         return new PostService(postRepository, categoryRepository);
+    }
+
+    @Bean
+    public CreatePostUseCase createPostUseCase(PostService postService) {
+        return postService;
+    }
+
+    @Bean
+    public ListPostsUseCase listPostsUseCase(PostService postService) {
+        return postService;
+    }
+
+    @Bean
+    public GetPostUseCase getPostUseCase(PostService postService) {
+        return postService;
+    }
+
+    @Bean
+    public UpdatePostUseCase updatePostUseCase(PostService postService) {
+        return postService;
+    }
+
+    @Bean
+    public DeletePostUseCase deletePostUseCase(PostService postService) {
+        return postService;
+    }
+
+    @Bean
+    public PublishPostUseCase publishPostUseCase(PostService postService) {
+        return postService;
+    }
+
+    @Bean
+    public ListCategoriesUseCase listCategoriesUseCase(CategoryService categoryService) {
+        return categoryService;
+    }
+
+    @Bean
+    public GetCategoryUseCase getCategoryUseCase(CategoryService categoryService) {
+        return categoryService;
+    }
+
+    @Bean
+    public UpdateCategoryUseCase updateCategoryUseCase(CategoryService categoryService) {
+        return categoryService;
+    }
+
+    @Bean
+    public DeleteCategoryUseCase deleteCategoryUseCase(CategoryService categoryService) {
+        return categoryService;
+    }
+
+    @Bean
+    public GetCategoryByPathUseCase getCategoryByPathUseCase(CategoryService categoryService) {
+        return categoryService;
     }
 }
--- src/main/java/com/thiago/abandonedcode/infra/config/CorsConfiguration.java
+++ src/main/java/com/thiago/abandonedcode/infra/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:4200")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
